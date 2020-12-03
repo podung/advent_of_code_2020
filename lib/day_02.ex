e8868ef9a4c8c4bc26c3dae960ceb048e599a7ca
Joe DePung <joe.depung@gmail.com>
2020-12-02 21:28:56 -0600

    Day 2, part 2

--- lib/day_02.ex
+++ lib/day_02.ex
@@ -7,6 +7,13 @@ defmodule Day02 do
     |> Enum.count()
   end
 
+  def part2(input) do
+    input
+    |> Enum.map(&parse_line/1)
+    |> Enum.reject(&violates_new_constraints/1)
+    |> Enum.count()
+  end
+
   defp parse_line(line) do
     [range_part, letter_part, password] = String.split(line, " ")
 
@@ -20,7 +27,7 @@ defmodule Day02 do
     {password, letter, index1, index2}
   end
 
-  def get_frequencies({password, letter, index1, index2}) do
+  defp get_frequencies({password, letter, index1, index2}) do
     frequencies =
       password
       |> String.graphemes()
@@ -31,5 +38,37 @@ defmodule Day02 do
 
   defp violates_constraints({_password, _letter, index1, index2, letter_count}) do
     letter_count < index1 || letter_count > index2
+  end
+
+  defp violates_new_constraints({password, letter, index1, index2}) do
+    first = String.at(password, index1 - 1)
+    second = String.at(password, index2 - 1)
+
+    first_position_match = first == letter
+    second_position_match = second == letter
+
+    #IO.puts " ********************"
+    #IO.inspect password
+    #IO.inspect letter
+    #IO.inspect "#{first} - #{index1}"
+    #IO.inspect "#{second} - #{index2}"
+
+    #IO.inspect first_position_match
+    #IO.inspect second_position_match
+    #IO.inspect first_position_match != second_position_match
+    #IO.puts " ********************\n\n\n"
+
+
+    !(first_position_match != second_position_match)
+  end
+
+    #[ index1, index2 ]
+    #|> Enum.map fn index ->
+      #password
+      #|> String.at(index1-1)
+      #|> Kernel.==(letter)
+    #end
+  defp position_match(string, position, letter) do
+
   end
 end
--- test/day_02_test.exs
+++ test/day_02_test.exs
@@ -15,12 +15,33 @@ defmodule Day02Test do
     assert Day02.part1(passwords) == 2
   end
 
-  test "calculates day1 part 1 correctly" do
+  test "calculates day2 part 1 correctly" do
     passwords =
       "lib/fixtures/day02_fixture.txt"
       |> File.read!()
       |> split_lines
 
     assert Day02.part1(passwords) == 414
+  end
+
+  test "calculates the sample values correctly for part2 correctly" do
+    passwords =
+      """
+      1-3 a: abcde
+      1-3 b: cdefg
+      2-9 c: ccccccccc
+      """
+      |> split_lines
+
+    assert Day02.part2(passwords) == 1
+  end
+
+  test "calculates day2 part 2 correctly" do
+    passwords =
+      "lib/fixtures/day02_fixture.txt"
+      |> File.read!()
+      |> split_lines
+
+    assert Day02.part2(passwords) == 413
   end
 end
